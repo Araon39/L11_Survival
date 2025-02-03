2812e37b236e009a2129d6682d93e32e8fbc29ef
Araon39 <115640752+Araon39@users.noreply.github.com>
2025-02-03 10:23:57 +0200

    Add Item
    Добавил еду воду и здоровье для удовлетворения потребностей

--- Assets/Scripts/HealthStatus.cs
+++ Assets/Scripts/HealthStatus.cs
@@ -14,6 +14,10 @@ public class HealthStatus : MonoBehaviour
     public Slider sliderWater;
     private float waterValue = 0.03f;
 
+    public float HPRecoveryAmount = 0.2f;
+    public float FoodRecoveryAmount = 0.3f;
+    public float WaterRecoveryAmount = 0.4f;
+
     void Update()
     {
         ChangeHP();
@@ -24,5 +28,23 @@ public class HealthStatus : MonoBehaviour
     void ChangeHP() { sliderHP.value -= HPValue * Time.deltaTime; }
     void ChangeFood() { sliderFood.value -= FoodValue * Time.deltaTime; }
     void ChangeWater() { sliderWater.value -= waterValue * Time.deltaTime; }
-    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HP"))
+        {
+            sliderHP.value += HPRecoveryAmount;
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Food"))
+        {
+            sliderFood.value += FoodRecoveryAmount;
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Water"))
+        {
+            sliderWater.value += WaterRecoveryAmount;
+            Destroy(other.gameObject);
+        }
+    }
 }
